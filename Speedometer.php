423eb2bd46f8e132404f9c1ec175c7b6f3ce2629
Guillaume CAVALIE <actix2013@gmail.com>
2020-04-26 20:50:52 +0200

    Correction  manque une fontion ,  ajout echo

--- Speedometer.php
+++ Speedometer.php
@@ -5,10 +5,15 @@ class Speedometer
 {
     const KM_TO_MILES = 0.621371;
 
-    public static function convertKmToMiles(int $kmh ){
-            return round(SELF::KM_TO_MILES * $kmh,2);
+    public static function convertKmToMiles(int $kmh)
+    {
+        return round(SELF::KM_TO_MILES * $kmh, 2);
+    }
 
-}
+    public static function convertMilesToKm(int $miles)
+    {
+        return round( $miles / SELF::KM_TO_MILES , 2);
+    }
 
 
 }
--- index.php
+++ index.php
@@ -1,5 +1,5 @@
 <?php
 require_once "Speedometer.php";
 
-echo Speedometer::convertKmToMiles(10);
-
+echo Speedometer::convertKmToMiles(10). " Miles <br>";
+echo Speedometer::convertMilesToKm(6.21) . "  ... oups probleme de round()<br>";
